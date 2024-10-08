9871320e9d1e60bee9abfa44c9013fe78e8114f9
aoyama01 <139197283+aoyama01@users.noreply.github.com>
2024-10-08 18:58:33 +0900

    fix: change single quote to double quote

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import { getAllPokemon, getPokemon } from './utils/pokemon';
-import Card from './components/Card';
-import Navbar from './components/Navbar';
+import { useEffect, useState } from "react";
+import "./App.css";
+import { getAllPokemon, getPokemon } from "./utils/pokemon";
+import Card from "./components/Card";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
   const [loading, setLoading] = useState(true);
-  const [pokemonData, setPokemonData] = useState([]); // _pokemonDataを入れたいので，空の配列で用意しておく
-  const [prevURL, setPrevURL] = useState(''); // 次のページのURLを格納するための状態変数を管理
-  const [nextURL, setNextURL] = useState(''); // 次のページのURLを格納するための状態変数を管理
+  const [pokemonData, setPokemonData] = useState([]); // _pokemonDataを入れたいので，空の配列を用意しておく
+  const [prevURL, setPrevURL] = useState(""); // 前のページのURLを格納するための状態変数を管理
+  const [nextURL, setNextURL] = useState(""); // 次のページのURLを格納するための状態変数を管理
 
   // データ読込時のローディング設定
   useEffect(() => {
@@ -45,19 +45,19 @@ function App() {
 
   // 前のページのデータを取得する関数
   const handlePrevPage = async () => {
-    console.log('handlePrevPageが発生');
+    console.log("handlePrevPageが発生");
     if (!prevURL) return; // prevURLが空の場合に早期リターン
 
     setLoading(true);
     try {
       let data = await getAllPokemon(prevURL); // 次のページのURLを格納するための状態変数が必要
-      console.log('dataを取得');
+      console.log("dataを取得");
       console.log(data);
       setPrevURL(data.previous);
       setNextURL(data.next);
       await loadPokemon(data.results);
     } catch (error) {
-      console.error('次のページの取得に失敗しました:', error);
+      console.error("次のページの取得に失敗しました:", error);
     } finally {
       setLoading(false); // ロードが完了
     }
@@ -65,19 +65,19 @@ function App() {
 
   // 次のページのデータを取得する関数
   const handleNextPage = async () => {
-    console.log('handleNextPageが発生');
+    console.log("handleNextPageが発生");
     if (!nextURL) return; // nextURLが空の場合に早期リターン
 
     setLoading(true);
     try {
       let data = await getAllPokemon(nextURL); // 次のページのURLを格納するための状態変数が必要
-      console.log('dataを取得');
+      console.log("dataを取得");
       console.log(data);
       setPrevURL(data.previous);
       setNextURL(data.next);
       await loadPokemon(data.results);
     } catch (error) {
-      console.error('次のページの取得に失敗しました:', error);
+      console.error("次のページの取得に失敗しました:", error);
     } finally {
       setLoading(false); // ロードが完了
     }
@@ -86,17 +86,17 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className='App'>
+      <div className="App">
         {loading ? (
           <h1>ロード中...</h1>
         ) : (
           <>
-            <div className='pokemonCardContainer'>
+            <div className="pokemonCardContainer">
               {pokemonData.map((pokemon, i) => {
                 return <Card key={i} pokemon={pokemon} />;
               })}
             </div>
-            <div className='btn'>
+            <div className="btn">
               <button onClick={handlePrevPage}>前へ</button>
               <button onClick={handleNextPage}>次へ</button>
             </div>
